b040b1f00560352d4d9e6d239fdb1b9117a5fed8
Noah Baron <32229274+noahbaron91@users.noreply.github.com>
2024-10-28 23:19:11 -0700

    fix: app

--- frontend-braintumor/src/App.vue
+++ frontend-braintumor/src/App.vue
@@ -1,19 +1,17 @@
 <script setup lang="ts">
-import { useTemplateRef, ref } from 'vue'
+import { useTemplateRef, ref, computed } from 'vue'
 import { config } from './config'
 
 const inputRef = useTemplateRef('file-input')
 
-type KeyOf<T> = T extends object ? keyof T : never
+const classification = ref<null | 0 | 1 | 2 | 3>(null)
 
-const classification = ref<null | KeyOf<typeof CLASS_MAPPING>>(null)
-
-const CLASS_MAPPING = {
-  0: 'Glioma',
-  1: 'Meningioma',
-  2: 'No Tumor',
-  3: 'Pituitary',
-} as const
+// const CLASS_MAPPING = {
+//   0: 'Glioma',
+//   1: 'Meningioma',
+//   2: 'No Tumor',
+//   3: 'Pituitary',
+// } as const
 
 const handleChange = async (event: Event) => {
   const file = (event.target as HTMLInputElement).files?.[0]
@@ -44,6 +42,23 @@ const handleChange = async (event: Event) => {
 const uploadImage = () => {
   inputRef.value?.click()
 }
+
+const label = computed(() => {
+  if (classification.value === null) return null
+
+  switch (classification.value) {
+    case 0:
+      return 'Glioma'
+    case 1:
+      return 'Meningioma'
+    case 2:
+      return 'No tumor'
+    case 3:
+      return 'Pituitary'
+  }
+
+  return null
+})
 </script>
 
 <template>
@@ -58,7 +73,7 @@ const uploadImage = () => {
       >Brain Tumor MRI Dataset</a
     >
   </p>
-  <p v-if="classification !== null">{{ CLASS_MAPPING[classification] }}</p>
+  <p v-if="label !== null">{{ label }}</p>
   <input
     type="file"
     accept="image/*"
